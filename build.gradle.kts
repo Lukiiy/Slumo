646ffbd2eeccf717269a86384b061edb322596f3
Lukiiy <lukiiy@proton.me>
2026-08-02 18:13:09 -0300

    init!!

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java-library")
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
+    id("com.gradleup.shadow") version "9.6.1"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    paperweight.paperDevBundle("26.1.1.build.+")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        val props = mapOf("version" to version, "description" to project.description)
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
+}
