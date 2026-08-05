e8e3f8f803ccdbc8c67ffbd9efd64eaff727cff2
Lukiiy <lukiiy@proton.me>
2026-08-05 16:51:35 -0300

    Update main class

--- src/main/java/me/lukiiy/slumo/Slumo.java
+++ src/main/java/me/lukiiy/slumo/Slumo.java
@@ -1,15 +1,29 @@
 package me.lukiiy.slumo;
 
+import me.lukiiy.flow.Flow;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+
 public final class Slumo extends JavaPlugin {
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-    }
+    private MapMaker mapMaker;
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onEnable() {
+        try {
+            mapMaker = new MapMaker();
+        } catch (IOException e) {
+            getLogger().severe("Couldn't setup the map maker! " + e.getMessage());
+        }
+
+        Flow.getInstance().getManager().register(new Entry());
+    }
+
+    public static Slumo getInstance() {
+        return JavaPlugin.getPlugin(Slumo.class);
+    }
+
+    public MapMaker getMapMaker() {
+        return mapMaker;
     }
 }
