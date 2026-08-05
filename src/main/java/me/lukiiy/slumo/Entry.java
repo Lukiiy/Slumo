b5e4fefd2a3464eceae3c95ef039c25703f7f140
Lukiiy <lukiiy@proton.me>
2026-08-05 17:04:35 -0300

    Add kits setting entry

--- src/main/java/me/lukiiy/slumo/Entry.java
+++ src/main/java/me/lukiiy/slumo/Entry.java
@@ -2,9 +2,21 @@ package me.lukiiy.slumo;
 
 import me.lukiiy.flow.FDefaults;
 import me.lukiiy.flow.GameEntry;
+import me.lukiiy.flow.setting.CycleSetting;
+import me.lukiiy.flow.setting.Option;
 import net.kyori.adventure.text.Component;
 
+import java.util.List;
+
 public class Entry extends GameEntry {
+    public final CycleSetting<Integer> tickRate = setting(new CycleSetting<>("tickrate", "Tickrate", "TPS during the match.", () -> List.of(new Option<>(5, "5 TPS"), new Option<>(8, "8 TPS"), new Option<>(10, "10 TPS"), new Option<>(15, "15 TPS"), new Option<>(20, "20 TPS"))));
+    public final CycleSetting<Kit> kit = setting(new CycleSetting<>("kit", "Kit", "", () -> List.of(
+            new Option<>(Kits.TROLL, Kits.TROLL.getName()),
+            new Option<>(Kits.PVP, Kits.PVP.getName()),
+            new Option<>(Kits.SUMO, Kits.SUMO.getName())
+    )));
+
+
     public Entry() {
         super("tasffa", "Slumo", Game::new, Component.text("Parkour Tag").color(FDefaults.LIME));
     }
