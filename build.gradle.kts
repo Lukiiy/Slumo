5f468c625e19b1180d9389da35e85c5f6badde30
Lukiiy <lukiiy@proton.me>
2026-08-05 13:42:56 -0300

    add engine + minor changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java-library")
+    java
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
     id("com.gradleup.shadow") version "9.6.1"
 }
@@ -10,11 +10,10 @@ repositories {
 
 dependencies {
     paperweight.paperDevBundle("26.1.1.build.+")
+    compileOnly(files("lib/paper.jar")) // engine
 }
 
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(25)
-}
+java.toolchain.languageVersion = JavaLanguageVersion.of(25)
 
 tasks {
     build {
