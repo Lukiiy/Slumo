1a60764c67371d3ace204b8e497fc0946fd92069
Lukiiy <lukiiy@proton.me>
2026-08-05 02:03:33 -0300

    Add main

---
+++ src/main/java/me/lukiiy/slumo/Slumo.java
@@ -0,0 +1,15 @@
+package me.lukiiy.slumo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Slumo extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
